d3d5d85a67f0ea1457c2d498807c9a3a5707a6a6
Moises Chavez <gchavez19956@gmail.com>
2024-03-08 02:05:13 -0500

    Add project files.

---
+++ Resources_Route_Payload_ApiRest/Controllers/ClientsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resources_Route_Payload_ApiRest.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Resources_Route_Payload_ApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private static List<Usuario> usuarios = new List<Usuario>
+        {
+        new Usuario{IdCliente=1, Name="Moises", Address="Martha de Rodos"},
+        new Usuario{IdCliente=2,Name="Jocelin", Address="Via Salitre"}
+
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> GetInformationClient()
+        {
+            return Ok(usuarios);
+        }
+
+        [HttpGet("id")]
+        public ActionResult<Usuario> GetUsuario(int id)
+        {
+             var usuario= usuarios.Find(x => x.IdCliente == id);
+            if (usuario == null)
+            {
+            return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult CrearUsuario([FromBody] Usuario usuario) 
+        {
+            usuarios.Add(usuario);
+            return CreatedAtAction(nameof(GetUsuario), new { id = usuario.IdCliente }, usuario);
+;                
+        }
+        [HttpPut("{id}")]
+        public IActionResult ActualizarUsuario(int id, [FromBody] Usuario usuario)
+        {
+            var usuarioExistente = usuarios.Find(u => u.IdCliente == id);
+            if (usuarioExistente == null)
+            {
+                return NotFound();
+            }
+
+            usuarioExistente.Name = usuario.Name;
+            usuarioExistente.Address = usuario.Address;
+            return NoContent();
+        }
+
+        
+        [HttpDelete("{id}")]
+        public IActionResult BorrarUsuario(int id)
+        {
+            var usuario = usuarios.Find(u => u.IdCliente == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            usuarios.Remove(usuario);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Resources_Route_Payload_ApiRest/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Resources_Route_Payload_ApiRest.Models
+{
+    public class Usuario
+    {
+        public int IdCliente { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
